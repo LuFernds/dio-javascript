e342858a0c8b1a6a046d964c687ebb82b9b774b1
LuFernds <lucianopsfernandes@gmail.com>
2023-12-03 20:03:27 -0300

    Diretórios 01 e 02

---
+++ 02 - condicionais/aula.js
@@ -0,0 +1,53 @@
+const numero = 10;
+const eNumeroPar = (numero % 2) === 0;
+
+if (eNumeroPar) {
+    console.log('O número é Par!');
+} else {
+    console.log('O número é Impar!');
+}
+
+
+
+/*
+const numero = 9;
+const numeroDivisivelPor5 = (numero % 5) === 0;
+
+if (numero === 0) {
+    console.log('O número é inválido!')
+} else if (numeroDivisivelPor5) {
+    console.log('Sim!');
+} else {
+    console.log('Não!');
+}
+*/
+
+
+/*
+const numero = 10;
+const numeroPar = (numero % 2) === 0;
+console.log(numeroPar);
+
+if(numeroPar){
+    console.log('Par!');
+}else{
+    console.log('Impar!');
+}
+*/
+
+/*
+usar == para comparar String com Int.
+const numeroPar = (numero % 2) == '0';
+*/
+
+/*
+if(numeroPar){
+    console.log('Par!');
+}
+
+
+if(!numeroPar){
+    console.log('Impar!');
+}
+*/
+
---
+++ 02 - condicionais/segundo_desafio.js
@@ -0,0 +1,28 @@
+/*
+Faça um programa para calcular o valor de uma viagem.
+
+Você terá 3 váriaveis, sendo elas:
+1 - Preço do etanol;
+2 - Preço da gasolina;
+3 - Tipo de combustivel que está no seu carro;
+2 - Gasto médio de combustível do carro por KM;
+3 - Distância em KM da viagem.
+
+Imprima no console o valor que será gasto de combustível para realizar esta viagem.
+*/
+
+const precoEtanol = 3.64;
+const precoGasolina = 5.62;
+const tipoCombustivel = 'gasolina';
+const kmPorLitro = 9.6;
+let distanciaKM = 403;
+
+let distanciaPorLitro = distanciaKM / kmPorLitro;
+
+if(tipoCombustivel === 'etanol'){
+    const valorGasto = distanciaPorLitro * precoEtanol;
+    console.log('tipo de carro: '+tipoCombustivel+', valor gasto: '+valorGasto.toFixed(2));
+}else{
+    const valorGasto = distanciaPorLitro * precoGasolina;
+    console.log('tipo de carro: '+tipoCombustivel+', valor gasto: '+valorGasto.toFixed(2));
+}
